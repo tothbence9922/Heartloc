8270c3497e8fe5c3dafc9315ac0701d442b48f41
Benvalent <benvalents@gmail.com>
2020-02-13 14:57:27 +0100

    added default package
    - added default package ('main')
    - tested on own pc

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Main {
+	public static void main(String[] args) {
+		D x = new D();
+		x.x = 4;
+		for (int y = 0; y < x.x ; y++)
+		System.out.println("qwe");
+	}
+}
+
