4fd47e4d5a179587e276e3e239f72580d86b9c83
gabbler97 <gabbler970@gmail.com>
2020-02-18 13:19:34 +0100

    test
    selló

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, Heartloc!");
+		System.out.println("Hello, Heartloc! En vagyok a fogyatekos srac!! :'P");
 	}
 }
 
