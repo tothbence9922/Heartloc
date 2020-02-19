b65ef38a56bc51c773e6c5eda179dc6e994c28b0
matekomenda <matekomenda@users.noreply.github.com>
2020-02-19 13:52:38 +0100

    test
    body

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello, Heartloc! En vagyok a fogyatekos srac!! :'P");
+		System.out.println("Hello, Heartloc! En vagyok a fogyatekos srac!! :'P , xd lol hahaha" );
 	}
 }
 
