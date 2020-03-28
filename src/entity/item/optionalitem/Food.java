35de04b5e33722b1524921f57eba17e75c49df1b
cserik <61547757+cserik@users.noreply.github.com>
2020-03-28 11:24:10 +0100

    Update Food.java

--- src/entity/item/optionalitem/Food.java
+++ src/entity/item/optionalitem/Food.java
@@ -6,4 +6,18 @@ public class Food implements OptionalItem {
 			System.out.println("Food :3");
 		return false;
 	}
+<<<<<<< Updated upstream
+=======
+
+	public void pickup() {
+		System.out.println("Food\tvoid pickup()\tparam: -");
+	}
+	
+	public void setIsVisible(boolean trueorfalse) {
+		boolean testVisible=trueorfalse;
+		if(trueorfalse==true) {
+			System.out.println("Food is visible");
+		}
+	}
+>>>>>>> Stashed changes
 }
