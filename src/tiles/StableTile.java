01108e1561f2e0de3ed699bcd269069adf429516
cserik <61547757+cserik@users.noreply.github.com>
2020-03-28 14:18:37 +0100

    Update StableTile.java

--- src/tiles/StableTile.java
+++ src/tiles/StableTile.java
@@ -2,6 +2,8 @@ package tiles;
 
 import java.util.ArrayList;
 
+import entity.Entity;
+import entity.player.Explorer;
 import entity.player.Player;
 
 public class StableTile extends Tile {
@@ -15,10 +17,25 @@ public class StableTile extends Tile {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public boolean receive(Explorer e){
+		System.out.println("StableTile\tboolean receive(Explorer)\tparam: ex");
+		return true;
+	}
+	
+	public boolean remove(Explorer e){
+		System.out.println("StableTile\tboolean remove(Explorer)\tparam: ex");
+		return true;
+	}
 
 	public void dig(int i) {
-		System.out.println("StableTile\tvoid dig(int)\tparam: " + i );
+		System.out.println("StableTile\tvoid dig(int)\tparam:" + i );
 		
+	}
+	
+	public boolean getHasHole(){
+		System.out.println("StableTile\tboolean getHasHole()\tparam: -");
+		return this.hasHole;
 	}
 
 	public void removeSnow(int i) {
