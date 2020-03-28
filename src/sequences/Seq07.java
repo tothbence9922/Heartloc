9b6d75f64d2b37e9ad626c72e3df17b361fab903
cserik <61547757+cserik@users.noreply.github.com>
2020-03-28 12:56:30 +0100

    Update Seq07.java

--- src/sequences/Seq07.java
+++ src/sequences/Seq07.java
@@ -1,5 +1,24 @@
 package sequences;
 
-public class Seq07 {
+import entity.item.optionalitem.Food;
+import entity.item.optionalitem.Shovel;
+import entity.player.Explorer;
+import tiles.StableTile;
 
+public class Seq07 {
+	
+	Explorer ex = new Explorer();
+	Food f = new Food();
+	public Seq07() {
+		ex.step();
+		System.out.println("Explorers energy : ");
+		ex.getEnergy();
+		if(ex.getEnergy()>0) {
+			f.eat();
+			ex.heal(1);
+			System.out.println("\t(heal(1) returned)");
+			System.out.println("\t(eat() returned)");
+		}
+		System.out.println("\t(step() returned)");
+	}
 }
