cc9be2bec258c38b7c4a1f922b5d02ff3365d372
tothbence9922 <tothbence9922@users.noreply.github.com>
2020-04-11 11:25:09 +0200

    MapLoader class created
    ...and added to the model

---
+++ src/model/MapLoader.java
@@ -0,0 +1,10 @@
+package model;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+import org.json.simple.parser.JSONParser;
+
+public class MapLoader {
+
+}
