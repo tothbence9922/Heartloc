d3838e1a28e3802204856e41d5ea6020317a2d0a
cserik <61547757+cserik@users.noreply.github.com>
2020-04-24 21:22:26 +0200

    player

--- src/entity/player/Eskimo.java
+++ src/entity/player/Eskimo.java
@@ -1,10 +1,11 @@
 package entity.player;
+
 import tiles.Tile;
 
 /**
- * A játékban szereplõ egyik entitás. Kezdésként 5 egységnyi testhõje van, 
- * valamint egy különleges képessége is: tud iglut építeni, amiben megbújva 
- * a hóvihar nem tesz kárt bennük.
+ * A játékban szereplõ egyik entitás. Kezdésként 5 egységnyi testhõje van,
+ * valamint egy különleges képessége is: tud iglut építeni, amiben megbújva a
+ * hóvihar nem tesz kárt bennük.
  */
 public class Eskimo extends Player {
 
@@ -12,20 +13,30 @@ public class Eskimo extends Player {
 		this.bodyTemperature = 5;
 		this.energy = 4;
 	}
-	
+
 	/**
-	 * bool buildIgloo(Tile chosenTile): Az Å‘sosztÃ¡lybÃ³l szÃ¡rmazÃ³ metÃ³dus, mely
-	 * itt az EszkimÃ³ kÃ©pessÃ©gÃ©t valÃ³sÃ­tja meg: Ã©pÃ­t egy iglut arra a jÃ©gtÃ¡blÃ¡ra, ahol
-	 * az EszkimÃ³ Ã¡ll.
+	 * bool buildIgloo(Tile chosenTile): Az Å‘sosztÃ¡lybÃ³l szÃ¡rmazÃ³ metÃ³dus,
+	 * mely itt az EszkimÃ³ kÃ©pessÃ©gÃ©t valÃ³sÃ­tja meg: Ã©pÃ­t egy iglut arra a
+	 * jÃ©gtÃ¡blÃ¡ra, ahol az EszkimÃ³ Ã¡ll.
+	 * 
 	 * @param t - Az a Tile ami Ã¡ll az EszkimÃ³
-	 * @return - A sikeresen felÃ©pÃ­tett Iglu esetÃ©n true, ellenkezÅ‘ esetben false.
+	 * @return - A sikeresen felÃ©pÃ­tett Iglu esetÃ©n true, ellenkezÅ‘ esetben
+	 *         false.
 	 */
+	@Override
 	public boolean buildIgloo(Tile chosenTile) {
-		//System.out.println("Eskimo\tvoid build()\tparam: " + chosenTile);
-		
+		if (energy > 0) {
+			Igloo igloo = new Igloo(chosenTile);
+			if (chosenTile.getCapacity() < chosenTile.load()) {
+				for (int i = 0; i < chosenTile.getPlayers().length; i++) {
+					chosenTile.getPlayers().at(i).pushToWater();
+				}
+			}
+			return true;
+		}
 		return false;
 	}
- 
+
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
--- src/entity/player/Explorer.java
+++ src/entity/player/Explorer.java
@@ -3,34 +3,37 @@ package entity.player;
 import tiles.Tile;
 
 /**
- * A játékban szereplõ egyik entitás. Kezdésként 4 egységnyi testhõje van, valamint
- * egy különleges képessége is: meg tudja nézni a legfeljebb 3 távolságra lévõ
- * jégtábláról, hogy mekkora a teherbírása. Ez fontos és hasznos képesség, ugyanis az
- * instabil jégtáblák nagy veszélyt jelentenek.
+ * A játékban szereplõ egyik entitás. Kezdésként 4 egységnyi testhõje van,
+ * valamint egy különleges képessége is: meg tudja nézni a legfeljebb 3
+ * távolságra lévõ jégtábláról, hogy mekkora a teherbírása. Ez fontos és hasznos
+ * képesség, ugyanis az instabil jégtáblák nagy veszélyt jelentenek.
  */
-public class Explorer extends Player{
+public class Explorer extends Player {
 
 	public Explorer() {
 		this.bodyTemperature = 4;
 		this.energy = 4;
 	}
-	
+
 	/**
-	 * Az õsosztályból származó metódus, mely itt az Explorer képességét valósítja meg: a
-	 * játékos pozíciójától legfeljebb 3 távolságra lévõ jégtáblák közül egyrõl 
-	 * megállapítja a teherbírását
+	 * Az õsosztályból származó metódus, mely itt az Explorer képességét valósítja
+	 * meg: a játékos pozíciójától legfeljebb 3 távolságra lévõ jégtáblák közül
+	 * egyrõl megállapítja a teherbírását
+	 * 
 	 * @param chosenTile a kiválasztott Tile
 	 * @return a Tile teherbírása
 	 */
 	@Override
 	public int exploreTile(Tile chosenTile) {
-		System.out.println("Explorer\tint ExploreTile()\tparam: " + chosenTile);
-		return 5;
+		if (chosenTile.getDistance() <= 3) {
+			return chosenTile.getCapacity();
+		}
+		return 0;
 	}
 
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
-		
+
 	}
 }
